8313df58ace3781cf90fc7bd7297d0fc813ea887
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-10-18 11:35:33 +0200

    update tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
   content: [
-    'components/**/*.{vue,js}',
     'storyblok/**/*.{vue,js}',
-    'layouts/**/*.vue',
+    'components/**/*.{vue,js}',
     'pages/**/*.vue',
-    'composables/**/*.{js,ts}',
-    'plugins/**/*.{js,ts}',
-    'App.{js,ts,vue}',
-    'app.{js,ts,vue}',
   ],
   safelist: ['bg-white'],
   theme: {
