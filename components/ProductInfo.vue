46dd895436bbbb0119503015407641118d5dcbbe
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-10-18 19:49:20 +0200

    fix: display product availability

--- components/ProductInfo.vue
+++ components/ProductInfo.vue
@@ -2,7 +2,7 @@
   <h4 class="text-xl font-bold text-[#1c243d] mb-3">
     {{ productTitle }}
   </h4>
-  <span v-if="!productAvailability" class="text-[#50b0ae] font-bold"
+  <span v-if="productAvailability" class="text-[#50b0ae] font-bold"
     >{{ productPrice }} EUR</span
   >
   <span v-else class="text-[#ed6c61]">Out of stock</span>
